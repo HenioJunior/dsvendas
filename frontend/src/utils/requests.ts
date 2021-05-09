3bc524e4e066c778d4a2700daf8a0c9955045ccf
Henio Junior <henioajunior@gmail.com>
2021-05-08 23:50:10 -0300

    Env var5

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,3 +1,1 @@
-// export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
-
-export const API_URL = process.env.REACT_APP_API_URL;
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'https://crystaldata-dsvendas.herokuapp.com';
